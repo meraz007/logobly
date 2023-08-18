e109c6fd2997c0e304076eeac04cb03ab8eaa596
Saiful Islam Meraz <42317343+meraz007@users.noreply.github.com>
2023-08-18 17:20:11 +0600

    added login and sign up page

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,6 +9,7 @@ const config: Config = {
   theme: {
     extend: {
       backgroundImage: {
+        'auth-cover': "url('../public/img/auth_cover.jpg')",
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
